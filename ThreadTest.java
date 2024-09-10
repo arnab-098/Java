4f68a8159d8384693c841b4060f0d0677242a1aa
arnab <arnab.chattopadhyay777@gmail.com>
2024-09-10 18:23:13 +0530

    thread synchronization for addition

--- ThreadTest.java
+++ ThreadTest.java
@@ -1,9 +1,32 @@
 
 import java.util.*;
 
+
+class Print {
+
+  private int s;
+
+  synchronized public int sum() {
+    s = 0;
+    for (int i=0; i<10; i++) {
+      s += i;
+      System.out.println(Thread.currentThread().getName() + " added " + i + "\tTotal: " + s);
+    }
+    try {Thread.sleep(10);}
+    catch (InterruptedException e) {
+      System.out.println(Thread.currentThread().getName() + " interrupted");
+    }
+    return s;
+  }
+
+}
+
+
 class myThread implements Runnable {
 
   public Thread th;
+  static Print p = new Print();
+  private int result;
 
   public myThread(String name) {
     this.th = new Thread(this, name);
@@ -12,32 +35,34 @@ class myThread implements Runnable {
 
   public void run() {
     System.out.printf("%s starting\n", this.th.getName());
-    try {
-      for (int i=0; i<5; i++) {
-        System.out.println(this.th.getName() + "[" + i + "]");
-        Thread.sleep(100);
-      }
-    }
-    catch (InterruptedException e) {
-      System.out.println(this.th.getName() + " interrupted");
-    }
+    this.result = p.sum();
+    System.out.println(this.th.getName() + " Result: " + result);
     System.out.println(this.th.getName() + " terminating");
+  }
+
+  public int getResult() {
+    return this.result;
   }
   
 }
 
+
 public class ThreadTest {
 
   public static void main (String[] args) {
-    myThread mt = new myThread("T1");
     System.out.println("Main thread running");
+    myThread th1 = new myThread("T1");
+    myThread th2 = new myThread("T2");
     try {
-      mt.th.join();
+      th1.th.join();
       System.out.println("T1 joined after termination");
+      th2.th.join();
+      System.out.println("T2 joined after termination");
     }
     catch (InterruptedException e) {
       System.out.println("Main thread interrupted");
     }
+    System.out.println("Result: " + (th1.getResult() + th2.getResult()));
     System.out.println("Main thread terminating");
   }
 
